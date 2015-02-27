e920939fe6ff7b87456b419487f607508daa9ef1
Jasper St. Pierre <jstpierre@mecheye.net>
2015-02-26 17:14:57 -0800

    two-step: Make SHOW_ANIMATION_PERCENT configurable
    We're going to use this to always show the end animation by setting to
    0.0.

--- src/plugins/splash/two-step/plugin.c
+++ src/plugins/splash/two-step/plugin.c
@@ -64,10 +64,6 @@
 #define FRAMES_PER_SECOND 30
 #endif
 
-#ifndef SHOW_ANIMATION_PERCENT
-#define SHOW_ANIMATION_PERCENT 0.9
-#endif
-
 typedef enum
 {
         PLY_BOOT_SPLASH_DISPLAY_NORMAL,
@@ -123,6 +119,7 @@ struct _ply_boot_splash_plugin
         uint32_t                            background_start_color;
         uint32_t                            background_end_color;
 
+        double                              show_animation_percent;
         progress_function_t                 progress_function;
 
         ply_trigger_t                      *idle_trigger;
@@ -557,6 +554,7 @@ create_plugin (ply_key_file_t *key_file)
         char *transition_duration;
         char *color;
         char *progress_function;
+        char *show_animation_percent;
 
         srand ((int) ply_get_timestamp ());
         plugin = calloc (1, sizeof(ply_boot_splash_plugin_t));
@@ -670,6 +668,13 @@ create_plugin (ply_key_file_t *key_file)
                         plugin->progress_function = PROGRESS_FUNCTION_TYPE_LINEAR;
                 }
         }
+
+        show_animation_percent = ply_key_file_get_value (key_file, "two-step", "ShowAnimationPercent");
+        if (show_animation_percent != NULL)
+                plugin->show_animation_percent = strtod (show_animation_percent, NULL);
+        else
+                plugin->show_animation_percent = .9;
+        free (show_animation_percent);
 
         plugin->views = ply_list_new ();
 
@@ -1170,7 +1175,7 @@ on_boot_progress (ply_boot_splash_plugin_t *plugin,
         if (plugin->is_idle)
                 return;
 
-        if (percent_done >= SHOW_ANIMATION_PERCENT) {
+        if (percent_done >= plugin->show_animation_percent) {
                 if (plugin->stop_trigger == NULL) {
                         ply_trace ("boot progressed to end");
 
@@ -1184,7 +1189,7 @@ on_boot_progress (ply_boot_splash_plugin_t *plugin,
         } else {
                 double total_duration;
 
-                percent_done *= (1 / SHOW_ANIMATION_PERCENT);
+                percent_done *= (1 / plugin->show_animation_percent);
 
                 switch (plugin->progress_function) {
                 /* Fun made-up smoothing function to make the growth asymptotic:
