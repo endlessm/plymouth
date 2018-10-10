cc2ab9c7fee0a558adddf4977ba8e1cb14d7bde2
Ray Strode <rstrode@redhat.com>
2018-10-10 20:07:37 +0100

    device-manager: don't watch for udev events when deactivated
    If a device gets added when we're already deactivated, plymouth shouldn't
    process the device, since processing it effectively activates plymouth.
    
    This commit pulls the udev monitor fd out of the event loop while
    plymouth is deactivated so new events are deferred until reactivation.
    
    Modified by Iain Lane <iain.lane@canonical.com>: Also deactivate the
    timer that finds all devices known to udev after an interval, when
    paused.

--- src/main.c
+++ src/main.c
@@ -1306,6 +1306,7 @@ on_deactivate (state_t       *state,
         ply_trace ("deactivating");
         cancel_pending_delayed_show (state);
 
+        ply_device_manager_pause (state->device_manager);
         ply_device_manager_deactivate_keyboards (state->device_manager);
 
         if (state->boot_splash != NULL) {
@@ -1339,6 +1340,8 @@ on_reactivate (state_t *state)
 
         ply_device_manager_activate_keyboards (state->device_manager);
         ply_device_manager_activate_renderers (state->device_manager);
+
+        ply_device_manager_unpause (state->device_manager);
 
         state->is_inactive = false;
 
